b69abdca418e9f03067bf5d8c00b168759efae9d
iangozl <ig_computing@protonmail.com>
2021-03-04 01:13:49 -0500

    Another commit for today!

--- Binary_search_algo.py
+++ Binary_search_algo.py
@@ -1,18 +1,22 @@
 """
-Create a random list of numbers between 0 and 100 with a difference of 2 between each number. Ask the user for a number between 0 
-and 100 to check whether their number is in the list. The programme should work like this. The programme will half the list of 
-numbers and see whether the users number matches the middle element in the list. If they do not match, the programme will check 
-which half the number lies in, and eliminate the other half. The search then continues on the remaining half, again checking 
-whether the middle element in that half is equal to the user’s number. This process keeps on going until the programme finds the 
-users number, or until the size of the subarray is 0, which means the users number isn't in the list.
-
+Create a random list of numbers between 0 and 100 with a difference of 2 between each number. 
+Ask the user for a number between 0 and 100 to check whether their number is in the list. 
+The programme should work like this. The programme will half the list of numbers and see whether 
+the users number matches the middle element in the list. If they do not match, the programme will 
+check which half the number lies in, and eliminate the other half. The search then continues on the 
+remaining half, again checking whether the middle element in that half is equal to the user’s number. 
+This process keeps on going until the programme finds the users number, or until the size of the 
+subarray is 0, which means the users number isn't in the list.
 """
 import random
 
+random_number = random.randint(0, 1)
 # Making a list of 100 RANDOM numbers
-number_list = [random.randint(0, 100) for x in range(100)]
 
-#print(*number_list)
+number_list = [x for x in range(random_number, 100, 2)] 
+random.shuffle(number_list) 
+
+print(*number_list)
 # print("Number of elements", len(number_list))
 
 list_length = len(number_list)
@@ -29,11 +33,19 @@ print("\nFirst Half:", *first_half)
 second_half = number_list[middle_index:]
 print("\nSecond Half:", *second_half)
 
+user_input = int(input("Enter a number between 0 and 100: "))
+
+# Check if the middle_index is the correct number
+if number_list[middle_index] == user_input:
+    print("CONGRATULATIONS! YOU GUESSED THE NUMBER!")
+
+else: 
+    print("Keep Trying!")
 
 # A counter
 
 counter = 0 
-
+"""
 # Trying to split the list 4 times
 for x in range(4):
     
@@ -44,3 +56,5 @@ for x in range(4):
     number_list = first_half
 
     counter +=1
+
+"""
