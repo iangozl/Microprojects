5cb8b1290b8099302b6e2f87a8168b0c01a7d107
iangozl <ig_computing@protonmail.com>
2021-03-12 22:39:00 -0500

    Last for today

--- Employees_database/employee.py
+++ Employees_database/employee.py
@@ -4,8 +4,8 @@ class Employee:
         self.first = first
         self.last = last
         self.pay = pay
-
-    @property #<= it allows you to access a method as if it was an attribute 
+    
+     #<= it allows you to access a method as if it was an attribute 
     
 
 
--- Employees_database/sqlite_demo.py
+++ Employees_database/sqlite_demo.py
@@ -1,18 +1,39 @@
 import sqlite3
 from employee import Employee
 
-conn = sqlite3.connect('Employees_database/employee.db') # Python creates the file automatically
+# conn = sqlite3.connect('Employees_database/employee.db') # Python creates the file automatically
+
+# CONNECTION USING MEMORY
+conn = sqlite3.connect(':memory:') # It starts fresh everytime it runs
 
 c = conn.cursor() # Now we can run SQL commands with the execute() method
 
-# c.execute("""CREATE TABLE employees (
-#             first text,
-#             last text,
-#             pay integer
-#             )""") # <- Using docstrings to use multiple lines for the query
+c.execute("""CREATE TABLE employees (
+            first text,
+            last text,
+            pay integer
+            )""") # <- Using docstrings to use multiple lines for the query
+
+def insert_emp(emp):
+    with conn: # WITH allows us not to write a commit function
+        c.execute("INSERT INTO employees VALUES (?, ?, ?)", (emp.first, emp.last, emp.pay))
+
+def get_emps_by_name(lastname):
+    c.execute("SELECT * FROM employees WHERE last = :last", {'last': lastname}) # Using a Dictionary 
+    return c.fetchall()
+
+def update_pay(emp,pay):
+    with conn:
+        c.execute(""" UPDATE employees SET pay = :pay WHERE first = :first AND last = :last""",
+                {'pay' : pay , 'first' : emp.first, 'last' : emp.last})
+
+def remove_emp(emp):
+    with conn:
+        c.execute("DELETE FROM employees WHERE first = :first AND last = :last", {'first': emp.first, 'last': emp.last})
+
 
 emp_1 = Employee('Ricardo', 'Ramirez', 10000)
-emp_2 = Employee('Juan','Perez', 20000)
+emp_2 = Employee('Juan','Ramirez', 20000)
 
 # c.execute("INSERT INTO employees VALUES ('Daniel', 'Lopez', 10000)")
 
@@ -23,25 +44,35 @@ emp_2 = Employee('Juan','Perez', 20000)
 # c.execute("INSERT INTO employee VALUES ('{}', '{}', '{}')".format(emp_1.first, emp_1.last, emp_1.pay)) <- Values can be changed
 
 # INSERT info into the DATABASE by using tuples
-c.execute("INSERT INTO employee VALUES (?, ?, ?)", (emp_1.first, emp_1.last, emp_1.pay))
-conn.commit()
+# c.execute("INSERT INTO employees VALUES (?, ?, ?)", (emp_1.first, emp_1.last, emp_1.pay))
+# conn.commit()
 
-# PROPER WAY to put the placeholder
-c.execute("INSERT INTO employee VALUES (:first, :last, :pay)", 
-            ('first': emp_1.first, 'last':emp_1.last, 'pay':emp_1.pay))
+# # PROPER WAY to put the placeholder
+# c.execute("INSERT INTO employees VALUES (:first, :last, :pay)", 
+#             {'first': emp_2.first, 'last':emp_2.last, 'pay':emp_2.pay})
+# conn.commit() 
 
-
-
-c.execute("SELECT * FROM employees WHERE last = 'Lopez'")
-
-
+# c.execute("SELECT * FROM employees WHERE last = ?", ('Lopez', )) # Using a tuple
 
 # Iterate THROUGH the result
 
-print(c.fetchall()) # All the results
+# print(c.fetchall()) # All the results
 
-conn.commit() # Commits the current transaction
+# c.execute("SELECT * FROM employees WHERE last = :last", ('Ramirez', )) # Using a Dictionary 
+# print(c.fetchall())
+
+# conn.commit() # Commits the current transaction
+
+insert_emp(emp_1)
+insert_emp(emp_2)
+
+emps = get_emps_by_name('Ramirez')
+print(emps)
+
+update_pay(emp_2, 30000)
+remove_emp(emp_1)
+
+emps = get_emps_by_name('Ramirez')
+print(emps)
 
 conn.close()
-
-
