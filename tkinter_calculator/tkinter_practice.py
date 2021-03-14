dcffea3bcf7f1dd6f9097820038c611084acb562
iangozl <ig_computing@protonmail.com>
2021-03-13 21:58:30 -0500

    That's it for today

---
+++ tkinter_calculator/tkinter_practice.py
@@ -0,0 +1,26 @@
+import tkinter
+
+window = tkinter.Tk()
+window.geometry("500x500")
+
+def hello():
+    print("Hello Mothafucka")
+
+def characters():
+    text = text_box.get()
+    label["text"] = text
+
+#Creating a label
+label = tkinter.Label(window)
+label.pack()
+
+button1 = tkinter.Button(window, text = "Click me!", command = characters)
+# Use lambda to use a function with parameters
+
+text_box = tkinter.Entry(window, font = "Helvetica")
+text_box.pack()
+
+button1.pack() # Appears in the screen without a specific direction
+ 
+window.mainloop() # This registers all that's happening inside the window
+
