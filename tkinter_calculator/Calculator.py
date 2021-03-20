d3fac2287aec29a0c71343e43d0e0f4f805657b9
iangozl <ig_computing@protonmail.com>
2021-03-20 16:26:35 -0500

    thanks

--- tkinter_calculator/Calculator.py
+++ tkinter_calculator/Calculator.py
@@ -6,8 +6,6 @@ root = Tk()
 # root.geometry("300x400")
 root.title("End Of The World Calc")
 
-number_1 = 0
-choice = 0
 # Functions
 
 def click(number):
@@ -15,38 +13,67 @@ def click(number):
     text_box.insert(END, number)
 
 def equals():
-    operations = [add, multiply]
-    global number_2 = int(text_box.get())
+    global number_2 
+    
+    number_2 = float(text_box.get())
 
     text_box.delete(0, END)
 
-    result =  
+    if operation == 0:
+        result = number_1 + number_2
 
-    
-    text_box.insert(0, result)    
+    if operation == 1:
+        result = number_1 * number_2
+
+    if operation == 2:
+        result = number_1 - number_2
+
+    if operation == 3:
+        result = number_1 / number_2
+    text_box.insert(0, result)
 
 def add():
-    number = int(text_box.get())
+    number = float(text_box.get())
+    
     global number_1
+    global operation
+
     number_1 = number
     text_box.delete(0, END)
-    return number_1 + number_2
+    operation = 0
 
 def clear():
     text_box.delete(0,END)
 
 def multiply():
     global number_1
-    number_1 = text_box.get()
+    global operation
+    number_1 = float(text_box.get())    
     text_box.delete(0,END)
+    operation = 1
 
+def subtract():
+    global number_1
+    global operation
+    number_1 = float(text_box.get())
+    text_box.delete(0,END)
+    operation = 2
+
+def divide():
+    global number_1
+    global operation
+    number_1 = float(text_box.get())
+    text_box.delete(0,END)
+    operation = 3
 
 # Buttons
 
-add_button = Button(root, text = "+", command = add, width= 10)
-equals_button = Button(root, text = "=", command = equals)
+add_button = Button(root, text = "+", command = add)
+equals_button = Button(root, text = "=", command = equals, width= 15, bg='green', fg='white')
 clear_button = Button(root, text = "C", command = clear)
 multiply_button = Button(root, text= "x", command = multiply)
+subtract_button = Button(root, text="-", command= subtract)
+divide_button = Button(root, text="/", command= divide)
 
 button1 = Button(root, text= "1", command = lambda: click(1))
 button2 = Button(root, text= "2", command = lambda: click(2))
@@ -61,12 +88,14 @@ button0 = Button(root, text= "0", command = lambda: click(0))
 
 # Pack
 
-add_button.grid(row=4, column=1, columnspan = 2)
-equals_button.grid(row=4, column=0)
-# clear_button.pack()
+clear_button.grid(row=1, column=6)
+divide_button.grid(row=1, column=4)
+multiply_button.grid(row=2, column=4)
+subtract_button.grid(row=3, column=4)
+add_button.grid(row=4, column=4)
+equals_button.grid(row=4, column=5, columnspan= 2)
+
 # button2.pack()
-
-
 
 button9.grid(row=1, column=0)
 button8.grid(row=1, column=1)
@@ -80,11 +109,11 @@ button3.grid(row=3, column=0)
 button2.grid(row=3, column=1)
 button1.grid(row=3, column=2)
 
-# button0.grid(row = , column =)
+button0.grid(row=4, column=0)
 
 # Text Box
-text_box = Entry(root)
+text_box = Entry(root, width=50)
 
-text_box.grid(row = 0, columnspan = 3)
+text_box.grid(row = 0, columnspan = 6)
 
 root.mainloop()
