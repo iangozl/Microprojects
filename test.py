5d27009eac9ce47f1f633c4e1d89792ea06a2624
chego <ianjobs@protonmail.com>
2025-03-01 20:33:06 -0500

    commit 3 from lenovo

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
-print("This is the catbranch")
-print("This line is written from a Lenovo T480")
-print("This is another line of code from a Lenovo T480")
+list_of_num = [1,2,3,4,5]
+
+for x in list_of_num:
+    print(f"If we add 22 to the number {x}, we get {x+22}")
